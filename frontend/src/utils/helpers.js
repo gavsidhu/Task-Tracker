65dd442bf18604740045fb9f3f552cb989e13b7c
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-04-20 16:55:24 -0500

    added functions to get, add, update, and delete tasks

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -14,3 +14,36 @@ export const sortTasks = (tasks, sortBy) => {
       }
     });
   }
+
+export const getAllTasks = async () => {
+    try {
+        const response = await axios.get("http://localhost:3001/tasks")
+    return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const addTask = async(task) => {
+    try {
+        await axios.post("http://localhost:3001/tasks", task)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const updateTask = async(taskId, updateData) => {
+    try {
+        await axios.patch(`http://localhost:3001/tasks/${taskId}`, updateData)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deleteTask = async(taskId) => {
+    try {
+        await axios.delete(`http://localhost:3001/tasks/${taskId}`)
+    } catch (error) {
+        console.error(error)
+    }
+}
