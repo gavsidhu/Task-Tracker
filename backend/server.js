bccb1d6e41c35f4570e4432afab7012f266d84c0
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-04-21 00:05:05 -0500

    moved server.js back to root

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Task Manager API" });
+});
+
+app.use("/tasks", require("./routes/tasks"));
+app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 3000;
+const server = require("http").createServer(app);
+
+if (process.env.NODE_ENV !== "test") {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}
+
+module.exports = server; // export the server instance
